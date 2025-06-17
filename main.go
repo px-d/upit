318bc470e6968e2ae2306540fd077372973cb6e9
Philip <44866668+px-d@users.noreply.github.com>
2025-06-17 08:29:24 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"strings"
+    "io"
+    "log"
+    "net/http"
+    "strings"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
 
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		bodyBytes, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
-			return
-		}
-		defer r.Body.Close()
-		bodyString := string(bodyBytes)
-		w.Write([]byte(strings.ToUpper(bodyString)))
-	})
+    r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+        defer r.Body.Close()
+        bodyBytes, err := io.ReadAll(r.Body)
+        if err != nil {
+            http.Error(w, "Error reading request body", http.StatusInternalServerError)
+            return
+        }
+        if len(bodyBytes) == 0 {
+            http.Error(w, "Empty request body", http.StatusBadRequest)
+            return
+        }
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.Write([]byte(strings.ToUpper(string(bodyBytes))))
+    })
 
-	http.ListenAndServe(":3009", r)
-
+    if err := http.ListenAndServe(":3009", r); err != nil {
+        log.Fatal(err)
+    }
 }
